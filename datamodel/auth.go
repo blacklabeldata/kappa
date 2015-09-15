651f7daa6687a542d8d16c0572b9209390b05b57
eliquious <eliquious@gmail.com>
2015-09-15 02:43:04 -0500

    Move PubliKeyCallback into server directory

--- datamodel/auth.go
+++ datamodel/auth.go
@@ -4,11 +4,7 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"crypto/subtle"
-	"errors"
-	"fmt"
 	"io"
-
-	"golang.org/x/crypto/ssh"
 )
 
 // SaltSize is the size of the salt for encrypting passwords
@@ -39,42 +35,4 @@ func SecureCompare(given, actual []byte) bool {
 
 	/* Securely compare actual to itself to keep constant time, but always return false */
 	return subtle.ConstantTimeCompare(actual, actual) == 1 && false
-}
-
-// PublicKeyCallback returns a function to validate public keys for user login.
-func PublicKeyCallback(sys System) (func(ssh.ConnMetadata, ssh.PublicKey) (*ssh.Permissions, error), error) {
-	if sys == nil {
-		return nil, errors.New("ssh server: System cannot be nil")
-	}
-
-	// Get user store
-	users, err := sys.Users()
-	if err != nil {
-		return nil, fmt.Errorf("ssh server: user store: %s", err)
-	}
-
-	return func(conn ssh.ConnMetadata, key ssh.PublicKey) (perm *ssh.Permissions, err error) {
-		// fmt.Println(string(key.Marshal()))
-
-		// Get user if exists, otherwise return error
-		user, err := users.Get(conn.User())
-		if err != nil {
-			return
-		}
-
-		// Check keyring for public key
-		if keyring := user.KeyRing(); !keyring.Contains(key.Marshal()) {
-			err = fmt.Errorf("invalid public key")
-			return
-		}
-
-		// Add pubkey and username to permissions
-		perm = &ssh.Permissions{
-			Extensions: map[string]string{
-				"pubkey":   string(key.Marshal()),
-				"username": conn.User(),
-			},
-		}
-		return
-	}, nil
 }
