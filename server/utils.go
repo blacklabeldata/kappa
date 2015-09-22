4e720d8d02207e9c929fdfeb14fc4958a8a61bf4
eliquious <eliquious@gmail.com>
2015-09-21 22:34:57 -0500

    Implement getKappaServer

--- server/utils.go
+++ server/utils.go
@@ -2,9 +2,12 @@ package server
 
 import (
 	"errors"
+	"fmt"
 	"net"
 	"os"
 	"path/filepath"
+
+	"strconv"
 
 	"github.com/hashicorp/serf/serf"
 )
@@ -36,8 +39,43 @@ func isKappaNode(member serf.Member) (bool, string) {
 
 // validateNode should validate all the Serf tags for the given member and returns
 // NodeDetails and any that occured error.
-func getKappaServer(serf.Member) (*NodeDetails, error) {
-	return &NodeDetails{}, errors.New("Not implemented")
+func getKappaServer(m serf.Member) (n *NodeDetails, err error) {
+
+	// Get node cluster
+	cluster, ok := m.Tags["cluster"]
+	if !ok {
+		err = errors.New("error: member missing cluster tag")
+		return
+	}
+
+	// Get node SSH port
+	port, ok := m.Tags["port"]
+	if !ok {
+		err = errors.New("error: member missing ssh port")
+		return
+	}
+
+	// Convert port to int
+	p, err := strconv.Atoi(port)
+	if err != nil {
+		err = fmt.Errorf("error: member ssh port cannot be converted to string: '%s'", port)
+		return
+	}
+
+	// Get node bootstrap
+	// All nodes which have this tag are bootstrapped
+	_, bootstrap := m.Tags["bootstrap"]
+
+	// Get SSH addr
+	addr := net.TCPAddr{IP: m.Addr, Port: p}
+
+	n = &NodeDetails{}
+	n.Name = m.Name
+	n.Cluster = cluster
+	n.SSHPort = p
+	n.Bootstrap = bootstrap
+	n.Addr = addr
+	return
 }
 
 // NodeDetails stores details about a single serf.Member
@@ -48,4 +86,8 @@ type NodeDetails struct {
 	Bootstrap bool
 	Addr      net.TCPAddr
 	Expect    int
+}
+
+func (n NodeDetails) String() string {
+	return fmt.Sprintf("%#v", n)
 }
